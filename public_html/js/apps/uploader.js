a097366d054750543aa9cb4e0c975430b3566286
紫珺 <@N551>
2016-08-28 02:36:06 +0800

    Merge origin/master
    Conflicts:
    	public_html/css/style.css
    	public_html/index.html

--- public_html/js/apps/uploader.js
+++ public_html/js/apps/uploader.js
@@ -1,79 +1,134 @@
 var uploader = function ($) {
-    var test = 1233333;
+
+    var showFileSize;
+    var fileCache = {};
+
     /**
      * 获取文件大小并转换成KB或MB
      * @param {type} byteSize:文件原始大小
      * @returns {String}:文件换算单位后的大小
      */
-    var dealFileSize = function (byteSize) {
-        var fileSize = Math.round(byteSize / 1024);
-        var suffix = 'KB';
-        if (fileSize > 1000) {
-            fileSize = Math.round(byteSize / 1000);
-            suffix = 'MB';
+    var convertFileSize = function (byteSize) {
+        var fileSize = (byteSize / 1024).toFixed(2);
+        var unit = 'KB';
+        if (fileSize > 1024) {
+            fileSize = (fileSize / 1024).toFixed(2);
+            unit = 'MB';
         }
-        return fileSize.toString() + suffix;
+        return {
+            size: fileSize,
+            unit: unit
+        };
     };
 
     /**
      *验证文件类型是否匹配
      * @param {type} type
      */
-    var dealFileType = function (type) {
-
+    var isRequiredTypeImage = function (type) {
+        switch (type) {
+            case 'image/jpeg':
+            case 'image/jpg':
+            case 'image/png':
+            case 'image/bmp':
+                return true;
+            default:
+                return false;
+        }
     };
 
     /**
      * 选择文件事件
      */
     $("#image_file2").change(function () {
-        var file = document.getElementById('image_file2').files;
-        var fileName = file[0].name;
-        var fileType = file[0].type;
-        var fileSize = file[0].size;
-        var width = $('.file-select-view').width();
+        fileCache = document.getElementById('image_file2').files;
+        var fileName = fileCache[0].name;
+        var fileType = fileCache[0].type;
+        var fileSize = fileCache[0].size;
 
-        $('.fileName').trigger('GET-FILESIZE', {size: fileSize});
-        $('.fileName').trigger('GET-FILETYPE', {type: fileType});
+        $('.file-name').trigger('GET-FILESIZE', {size: fileSize});
+        $('.file-name').trigger('GET-FILETYPE', {type: fileType});
 
-        $('.process-view').css('display', 'block');
-        $('.file-cancle').css('display', 'block');
-        $('.process-bar').css('width', width);
-        $('.fileName').text(fileName + "(" + fileSize + ")");
+        $('.file-info-panel').css('display', 'block');
+        $('.file-info-panel').css('border', 'solid 1px #E5E5E5');
+        $('.file-info-panel').css('padding', '10px');
+        $('.file-cancle').append('<a href="javascript:void(0)"></a>');
+        $('.file-cancle').addClass('file-close');
+
+        $('.file-name').text(fileName + "(" + showFileSize.size.toString() + showFileSize.unit + ")");
     });
 
-
-    $('#image_file2').click(function () {
-        $.ajax({
-            type: 'get',
-            url: 'resp.json',
-            success: function (data, textStatus, jqXHR) {
-                if (textStatus === 'success') {
-                    console.log('upload success')
-                }
-            }
-        });
-    });
+    /**
+     * 点击上传按钮事件
+     */
     $('.upload').click(function () {
-        console.log($('#image_file2').val())
-        $('.process-bar').css('background-color', 'green');
+        if ($.isEmptyObject(fileCache)) {
+            $('.file-name').trigger('NOT-SELECTED-FILE');
+        } else {
+            $.ajax({
+                type: 'get',
+                url: 'resp.json',
+                success: function (data, textStatus, jqXHR) {
+                    if (textStatus === 'success') {
+                        var width = $('.file-select-view').width() - 20;
+                        $('.process-bar').css('width', width);
+                        $('.process-bar').css('background-color', 'green');
+                        $('.process-bar').css('transition', 'background-color 2s ease,width 2s ease');
+
+                        setTimeout(function () {
+                            $('.upload-status').text("上传完成！");
+                            console.log('upload success');
+                        }, 2000);
+
+                        setTimeout(function () {
+                            $(".file-info-panel").fadeOut(300);
+                            $('.process-bar').css('background-color', '#E5E5E5');
+                            $('.process-bar').css('width', 0);
+                            $('.upload-status').text('');
+                        }, 3000);
+                    }
+                }
+            });
+            fileCache = {};
+        }
     });
+
+    /**
+     * 关闭文件信息窗口事件
+     */
     $('.file-cancle').click(function () {
-        $('.process-view').css('display', 'none');
+        $('.file-info-panel').fadeOut(1000);
     });
 
-    $('.fileName').bind('GET-FILESIZE', function (evt, param) {
+    /**
+     * 添加事件：是否选择文件
+     */
+    $('.file-name').bind('NOT-SELECTED-FILE', function () {
+        alert("请选择文件后点击上传！");
+    });
+
+    /**
+     * 添加事件：换算文件大小单位，并判断文件是否大于指定大小
+     */
+    $('.file-name').bind('GET-FILESIZE', function (evt, param) {
         console.log('get file size:' + param.size);
-        var showFileSize = dealFileSize(param.size);
-    });
-    $('.fileName').bind('GET-FILETYPE', function (evt, param) {
-        console.log('get file type:' + param.type);
-        var showFileSize = dealFileSize(param.type);
+        var fileSize = convertFileSize(param.size);
+        showFileSize = fileSize;
+        if (fileSize.unit === 'MB' && fileSize.size > 5) {
+            alert("图片大小超过5M！");
+        }
     });
 
-    return{
-        test: test
-    };
+    /**
+     * 添加事件：匹配文件类型
+     */
+    $('.file-name').bind('GET-FILETYPE', function (evt, param) {
+        console.log('get file type:' + param.type);
+        if (!isRequiredTypeImage(param.type)) {
+            alert("请上传jpeg、jpg、png、bmp格式的图片！");
+        }
+    });
+
 }(jQuery);
 
 
